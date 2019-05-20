6d4050b37dd269f43b1fb59a4c9faa54d9ea2d58
Sunny Singh <sunny@doeity.com>
2019-05-20 11:27:57 +0530

    Update Intrinsic.py

--- Intrinsic.py
+++ Intrinsic.py
@@ -1,7 +1,4 @@
-#Web Traffic Time Series Forecasting
-#Forecast future traffic to Wikipedia pages
-
-#https://github.com/JEddy92/TimeSeries_Seq2Seq/blob/master/notebooks/TS_Seq2Seq_Intro.ipynb
+#git clone
 #%cd ..
 #!mkdir .kaggle
 
@@ -11,5 +8,5 @@
 #!chmod 600 /root/.kaggle/kaggle.json
 #!kaggle competitions download -c web-traffic-time-series-forecasting
 
-!unzip household_power_consumption.zip
-!ls
+#!unzip household_power_consumption.zip
+#!ls
