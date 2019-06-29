f46091180c5f1ac04cf7d8a36c08e813780de38f
Sunny Singh <sunny@doeity.com>
2019-06-29 10:45:41 +0530

    Update Intrinsic.py

--- Intrinsic.py
+++ Intrinsic.py
@@ -1,6 +1,10 @@
 #git clone
+
 #%cd ..
+#%cd ./root
+
 #!mkdir .kaggle
+#%cd ./.kaggle
 
 #from google.colab import files
 #up = files.upload()
@@ -9,4 +13,5 @@
 #!kaggle competitions download -c web-traffic-time-series-forecasting
 
 #!unzip household_power_consumption.zip
+#!7z e train.json.7z
 #!ls
